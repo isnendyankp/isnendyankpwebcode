8fdceffbb40fa54061def1268addca928f1c3f64
Isnendyan <Isnendyankp@gmail.com>
2025-03-19 11:52:31 +0700

    feat: passing herosection component to homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import Herosection from "./components/Herosection";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
-      
+      <Herosection />
     </main>
   );
 }
