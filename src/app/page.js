991662bcbb8c88f8b1dd77c1cf12d7306e0c96f9
Isnendyan <Isnendyankp@gmail.com>
2025-03-20 18:50:08 +0700

    feat: restyling main container mx auto for main page

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Herosection from "./components/Herosection";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212]">
+    <main className="flex min-h-screen flex-col bg-[#121212] container mx-auto px-12 py-4">
       <Herosection />
     </main>
   );
