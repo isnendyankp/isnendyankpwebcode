7a08f7f7924fbe80de21e35374a9e19a6dbb290d
Isnendyan <Isnendyankp@gmail.com>
2025-03-20 18:51:36 +0700

    feat: add base about for herosection

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -4,6 +4,7 @@ const Herosection = () => {
   return (
     <section>
       <h1 className="text-white">Hello, I'm Nendy</h1>
+      <p className="text-white">I'm a software engineer based in Indonesia</p>
     </section>
   )
 }
