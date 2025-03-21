3e2079399ac6bad0b51154665ed56e30d5ffb11f
Isnendyan <Isnendyankp@gmail.com>
2025-03-21 16:52:52 +0700

    feat: add base for profile image

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -9,6 +9,13 @@ const Herosection = () => {
                 <h1 className="text-white mb-4 text-6xl font-extrabold">Hello, I'm Nendy</h1>
                 <p className="text-[#ADB7BE] text-lg lg:text-xl">I'm a software engineer based in Indonesia</p>
             </div>
+
+            {/* image section */}
+            <div className="col-span-5">
+                <div>
+                    
+                </div>
+            </div>
         </div>
     </section>
   )
