d2fae8c329b19989bf1b1e9e52cab8cc88a73260
Isnendyan <Isnendyankp@gmail.com>
2025-03-21 15:44:42 +0700

    feat: restyling change text size for about section

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -4,9 +4,9 @@ const Herosection = () => {
   return (
     <section>
       <h1 className="text-white mb-4 text-6xl font-extrabold">Hello, I'm Nendy</h1>
-      <p className="text-[#ADB7BE]">I'm a software engineer based in Indonesia</p>
+      <p className="text-[#ADB7BE] text-lg lg:text-xl">I'm a software engineer based in Indonesia</p>
     </section>
   )
 }
 
-export default Herosection
+export default Herosection 
