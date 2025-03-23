72f6bdf143815102c1cc624894e74bd8e1f95523
Isnendyan <Isnendyankp@gmail.com>
2025-03-23 16:08:45 +0700

    feat: add margin for about profile section

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -8,7 +8,7 @@ const Herosection = () => {
             {/* Section Left */}
             <div className="col-span-7 place-self-center">
                 <h1 className="text-white mb-4 text-6xl font-extrabold">Hello, I'm Nendy</h1>
-                <p className="text-[#ADB7BE] text-lg lg:text-xl">I'm a software engineer based in Indonesia</p>
+                <p className="text-[#ADB7BE] text-lg mb-6 lg:text-xl">I'm a software engineer based in Indonesia</p>
             </div>
 
             {/* image section */}
