c4d7afb08ea67f717a846c4436c6b089b8b63d5f
Isnendyan <Isnendyankp@gmail.com>
2025-03-24 15:28:02 +0700

    feat: add wrap container for herosection

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,10 @@ import Herosection from "./components/Herosection";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212] container mx-auto px-12 py-4">
-      <Herosection />
+    <main className="flex min-h-screen flex-col bg-[#121212]">
+      <div className="container mx-auto px-12 py-4">
+        <Herosection />
+      </div>
     </main>
   );
 }
