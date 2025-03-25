d93dfe359840f3a2f5bf0f688eec68561c68dc6c
Isnendyan <Isnendyankp@gmail.com>
2025-03-25 15:05:43 +0700

    feat: add width  at button herosection

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -10,10 +10,10 @@ const Herosection = () => {
                 <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">Hello, I'm Nendy</h1>
                 <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">I'm a software engineer based in Indonesia</p>
                 <div>
-                    <button className="px-6 py-3 rounded-full mr-4 bg-white text-black hover:bg-slate-200">
+                    <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-white text-black hover:bg-slate-200">
                         Hire Me
                     </button>
-                    <button className="px-6 py-3 rounded-full bg-transparent text-white border border-white mt-3">
+                    <button className="px-6 py-3 w-full sm:w-fit rounded-full bg-transparent text-white border border-white mt-3">
                         Download CV
                     </button>
                 </div>
