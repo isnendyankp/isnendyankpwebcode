e8b42ecd6490da9ae06dba674b6fc070afee4e0d
Isnendyan <Isnendyankp@gmail.com>
2025-03-26 15:17:48 +0700

    feat: remove border with colour for button download cv

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -18,7 +18,7 @@ const Herosection = () => {
                     <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-blue-700 to-indigo-300 text-white hover:bg-slate-200">
                         Hire Me
                     </button>
-                    <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-blue-700 to-indigo-300 text-white border border-white mt-3">
+                    <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-blue-700 to-indigo-300 text-white mt-3">
                         <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                             Download CV
                         </span>
