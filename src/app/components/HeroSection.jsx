dd6ea7e9437c311c6f220688e4e12f89636402c4
Isnendyan <Isnendyankp@gmail.com>
2025-03-28 13:25:35 +0700

    feat: add type animation with roles job profile

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,5 +1,7 @@
+"use client"
 import React from 'react'
 import Image from 'next/image'
+import { TypeAnimation } from "react-type-animation";
 
 const Herosection = () => {
   return (
@@ -9,7 +11,21 @@ const Herosection = () => {
             <div className="col-span-7 place-self-center text-center sm:text-left">
                 <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
                     <span className='text-transparent bg-clip-text  bg-gradient-to-br from-blue-500 via-blue-700 to-indigo-300'>Hello, I'm{" "}</span>
-                    Nendy
+                    <TypeAnimation
+                            sequence={[
+                                "Nendy",
+                                1000,
+                                "Software Engineer",
+                                1000,
+                                "Frontend Developer",
+                                1000,
+                                "QA Automation",
+                                1000,
+                            ]}
+                            wrapper="span"
+                            speed={50}
+                            repeat={Infinity}
+                    />  
                 </h1>
                 <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
                     I'm a software engineer based in Indonesia
