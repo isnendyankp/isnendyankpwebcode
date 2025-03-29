d930a4f088908c8e3c77451d0bceff724cbfd3ec
Isnendyan <Isnendyankp@gmail.com>
2025-03-29 17:08:52 +0700

    feat: add style flex for wrapper nav

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <nav>
-        <div>
+        <div classname="flex flex-wrap">
             <Link href="/" className="text-5xl text-white font-semibold">LOGO</Link>
             <div classname="menu"></div>
         </div>
