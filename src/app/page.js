8b362a8e91a79dc5fb2a54d2bb4d89607589d50c
Isnendyan <Isnendyankp@gmail.com>
2025-03-29 17:11:08 +0700

    feat: passing navbar into homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Herosection from "./components/Herosection";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
       <div className="container mx-auto px-12 py-4">
+        <Navbar />
         <Herosection />
       </div>
     </main>
