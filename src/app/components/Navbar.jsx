0afa2186ad42e9819efc799b63f8b05619a4d639
Isnendyan <Isnendyankp@gmail.com>
2025-04-01 17:11:21 +0700

    feat: add styling padding 8 for wrapper at navbar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 const Navbar = () => {
   return (
     <nav>
-        <div className="flex flex-wrap items-center justify-between mx-auto">
+        <div className="flex flex-wrap items-center justify-between mx-auto p-8">
             <Link href="/" className="text-5xl text-white font-semibold">LOGO</Link>
             <div className="menu"></div>
         </div>
