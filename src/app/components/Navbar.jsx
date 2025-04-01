84852e6175e279ed60919036024ca48db56e76cb
Isnendyan <Isnendyankp@gmail.com>
2025-04-01 17:05:27 +0700

    feat: add styling item center for wrapper at navbar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 const Navbar = () => {
   return (
     <nav>
-        <div className="flex flex-wrap">
+        <div className="flex flex-wrap item-center">
             <Link href="/" className="text-5xl text-white font-semibold">LOGO</Link>
             <div className="menu"></div>
         </div>
