4d0c4bb73a04ae890c902e4fb2cbf2037f90217a
Isnendyan <Isnendyankp@gmail.com>
2025-04-01 17:10:27 +0700

    feat: add styling margin auto for wrapper at navbar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 const Navbar = () => {
   return (
     <nav>
-        <div className="flex flex-wrap items-center justify-between">
+        <div className="flex flex-wrap items-center justify-between mx-auto">
             <Link href="/" className="text-5xl text-white font-semibold">LOGO</Link>
             <div className="menu"></div>
         </div>
