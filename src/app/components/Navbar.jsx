099e2a991e9c3be942164bbabb6931c8fb55d48c
Isnendyan <Isnendyankp@gmail.com>
2025-04-02 05:28:14 +0700

    feat: add ul and list for home

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -6,7 +6,11 @@ const Navbar = () => {
     <nav>
         <div className="flex flex-wrap items-center justify-between mx-auto p-8">
             <Link href="/" className="text-5xl text-white font-semibold">LOGO</Link>
-            <div className="menu"></div>
+            <div className="menu">
+                <ul>
+                    <li><Link href="/">Home</Link></li>
+                </ul>
+            </div>
         </div>
     </nav>
   )
