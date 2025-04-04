dfdfe6a9d96a0c9c542aacd729c184b314d8e094
Isnendyan <Isnendyankp@gmail.com>
2025-04-05 06:00:31 +0700

    feat: maping navlinks for navbar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -19,9 +19,10 @@ const Navbar = () => {
         <div className="flex flex-wrap items-center justify-between mx-auto p-8">
             <Link href="/" className="text-5xl text-white font-semibold">LOGO</Link>
             <div className="menu block md:w-auto" id="navbar">
-                <ul>
-                    <li><Link href="/" className="block py-2 pl-3 pr-4 text-white sm:text-xl rounded md:p-0 hover:text-gray-300">Home</Link></li>
-                    <li><Link href="#about" className="block py-2 pl-3 pr-4 text-white sm:text-xl rounded md:p-0 hover:text-gray-300">About</Link></li>
+                <ul className="flex flex-col md:flex-row space-y-8 md:space-y-0">
+                    {navLinks.map((link) => (
+                        <NavLink key={link.title} href={link.href} title={link.title} />
+                    ))}
                 </ul>
             </div>
         </div>
