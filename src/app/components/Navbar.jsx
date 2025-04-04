4693a9ebc6b6efb1a93e75f2b00eafceb63e8d91
Isnendyan <Isnendyankp@gmail.com>
2025-04-05 05:56:14 +0700

    feat: remove styling hidden for wrapper navbar list

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <nav>
         <div className="flex flex-wrap items-center justify-between mx-auto p-8">
             <Link href="/" className="text-5xl text-white font-semibold">LOGO</Link>
-            <div className="menu hidden md:block md:w-auto" id="navbar">
+            <div className="menu block md:w-auto" id="navbar">
                 <ul>
                     <li><Link href="/" className="block py-2 pl-3 pr-4 text-white sm:text-xl rounded md:p-0 hover:text-gray-300">Home</Link></li>
                     <li><Link href="#about" className="block py-2 pl-3 pr-4 text-white sm:text-xl rounded md:p-0 hover:text-gray-300">About</Link></li>
