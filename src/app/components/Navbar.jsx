8bbd145b46e057c7b17b3b325bc8d6e4b90b3433
Isnendyan <Isnendyankp@gmail.com>
2025-04-04 18:37:22 +0700

    feat: hover text colour for link home and about navbar list

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -8,8 +8,8 @@ const Navbar = () => {
             <Link href="/" className="text-5xl text-white font-semibold">LOGO</Link>
             <div className="menu hidden md:block md:w-auto" id="navbar">
                 <ul>
-                    <li><Link href="/" className="block py-2 pl-3 pr-4 text-white sm:text-xl rounded md:p-0">Home</Link></li>
-                    <li><Link href="#about" className="block py-2 pl-3 pr-4 text-white sm:text-xl rounded md:p-0">About</Link></li>
+                    <li><Link href="/" className="block py-2 pl-3 pr-4 text-white sm:text-xl rounded md:p-0 hover:text-gray-300">Home</Link></li>
+                    <li><Link href="#about" className="block py-2 pl-3 pr-4 text-white sm:text-xl rounded md:p-0 hover:text-gray-300">About</Link></li>
                 </ul>
             </div>
         </div>
