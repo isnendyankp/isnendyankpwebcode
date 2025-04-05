5c9eb86c5c52b762ec87c1400ea83586d4b05b0e
Isnendyan <Isnendyankp@gmail.com>
2025-04-06 06:57:38 +0700

    feat: fix maping navlinks for navbar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -28,9 +28,11 @@ const Navbar = () => {
             <Link href="/" className="text-5xl text-white font-semibold">LOGO</Link>
             <div className="menu block md:w-auto" id="navbar">
                 <ul className="flex flex-col md:flex-row space-y-8 md:space-y-0">
-                    {navLinks.map((link) => (
-                        <NavLink key={link.title} href={link.href} title={link.title} />
-                    ))}
+                {navLinks.map((link, index) => (
+                    <li key={index}>
+                        <NavLink href={link.href} title={link.title} />
+                    </li>
+                ))}  
                 </ul>
             </div>
         </div>
