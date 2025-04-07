cec6a2fc4baf7490e7be243eaf98f8e8326f25ef
Isnendyan <Isnendyankp@gmail.com>
2025-04-08 06:15:53 +0700

    feat: change md for text 5xl for tag link

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
   return (
     <nav>
         <div className="flex flex-wrap items-center justify-between mx-auto p-8">
-            <Link href="/" className="text-5xl text-white font-semibold">LOGO</Link>
+            <Link href="/" className="md:text-5xl text-white font-semibold">LOGO</Link>
             <div className="menu block md:w-auto" id="navbar">
                 <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
                     {navLinks.map((link, index) => (
