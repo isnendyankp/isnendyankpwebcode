525ba7ee2c856d062d3ea5f8c4bbf7b9aca3f39b
Isnendyan <Isnendyankp@gmail.com>
2025-04-10 05:31:10 +0700

    feat:add hidden for menu list navbar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
     <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90">
         <div className="flex flex-wrap items-center justify-between mx-auto px-4">
             <Link href="/" className="text-2xl md:text-5xl text-white font-semibold">LOGO</Link>
-            <div className="menu block md:w-auto" id="navbar">
+            <div className="menu hidden md:block md:w-auto" id="navbar">
                 <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
                     {navLinks.map((link, index) => (
                         <li key={index}>
