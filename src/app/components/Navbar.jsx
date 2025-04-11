5a2f5d35c24f0e6c5fdb4c69d5dbc6c323504210
Isnendyan <Isnendyankp@gmail.com>
2025-04-11 22:13:18 +0700

    feat:add block for mobile menu navbar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
     <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90">
         <div className="flex flex-wrap items-center justify-between mx-auto px-4">
             <Link href="/" className="text-2xl md:text-5xl text-white font-semibold">LOGO</Link>
-            <div className="mobile-menu">
+            <div className="mobile-menu block">
 
             </div>
             <div className="menu hidden md:block md:w-auto" id="navbar">
