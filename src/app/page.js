5d050eb633eec388efa80a6efb256a49035fcbf7
Isnendyan <Isnendyankp@gmail.com>
2025-04-16 17:55:39 +0700

    feat:passing about section component into page

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import Herosection from "./components/Herosection";
+import AboutSection from "./components/AboutSection";
 import Navbar from "./components/Navbar";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
       <Navbar />
       <div className="container mt-24 mx-auto px-12 py-4">
         <Herosection />
+        <AboutSection />
       </div>
     </main>
   );
