00f1cf6c6a80fa5b4dfffabb60673e151f673093
Isnendyan <Isnendyankp@gmail.com>
2025-04-18 15:53:11 +0700

    feat:add about me h2 at about section

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -6,6 +6,11 @@ const AboutSection = () => {
     <section className="text-white" id="about">
         <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16 ">
             <Image src="/images/about-image.png" width={500} height={500} />
+            <div>
+                <h2>
+                    About Me
+                </h2>
+            </div>
         </div>
     </section>
   )
