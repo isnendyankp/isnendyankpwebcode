3871bd0a5946916c415e6e912c0659424de94040
Isnendyan <Isnendyankp@gmail.com>
2025-04-18 15:58:52 +0700

    feat: styling margin bottom for h2 at about section

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -7,7 +7,7 @@ const AboutSection = () => {
         <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16 ">
             <Image src="/images/about-image.png" width={500} height={500} />
             <div>
-                <h2 className="text-4xl font-bold text-white">
+                <h2 className="text-4xl font-bold text-white mb-4">
                     About Me
                 </h2>
                 <p>
