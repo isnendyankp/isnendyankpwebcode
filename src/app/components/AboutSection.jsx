6e6f2f80bd8720856aebc1d404cc2989f3309035
Isnendyan <Isnendyankp@gmail.com>
2025-04-21 20:43:03 +0700

    feat: initiate tab skills with usestate at about section

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import Image from 'next/image'
+import { useState } from 'react'
 
 const AboutSection = () => {
+  const [tab, setTab] = useState("skills")
+
   return (
     <section className="text-white" id="about">
         <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16 ">
