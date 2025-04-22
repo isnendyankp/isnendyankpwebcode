0baeb7696452e1e6b1e8d8d3cbffad0236a0d748
Isnendyan <Isnendyankp@gmail.com>
2025-04-22 17:38:01 +0700

    feat: passing children into button at button component

--- src/app/components/TabButton.jsx
+++ src/app/components/TabButton.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const TabButton = ({ active, selectTab, children }) => {
   return (
-    <button className="mr-3 font-semibold hover:text-white text-[#ADB7BE] border-purple-500">Skills</button>
+    <button className="mr-3 font-semibold hover:text-white text-[#ADB7BE] border-purple-500">{children}</button>
   )
 }
 
