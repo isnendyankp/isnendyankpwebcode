91476537a99a7c3979d96973f877f7c8901ef6e2
Isnendyan <Isnendyankp@gmail.com>
2025-04-22 17:42:23 +0700

    feat: initiate button classes function at button component

--- src/app/components/TabButton.jsx
+++ src/app/components/TabButton.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
 const TabButton = ({ active, selectTab, children }) => {
+    const buttonClasses = active ? "text-white" : "text-[#ADB7BE]";
+
   return (
-    <button className="mr-3 font-semibold hover:text-white text-[#ADB7BE] border-purple-500">{children}</button>
+    <button className={`mr-3 font-semibold hover:text-white ${buttonClasses} border-purple-500`}>
+        {children}
+    </button>
   )
 }
 
