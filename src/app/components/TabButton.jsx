20e14d51a013770db1cef83877b0e57e409f0bf2
Isnendyan <Isnendyankp@gmail.com>
2025-04-22 17:35:44 +0700

    feat: add parameters at button component

--- src/app/components/TabButton.jsx
+++ src/app/components/TabButton.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const TabButton = () => {
+const TabButton = ({ active, selectTab, children }) => {
   return (
     <button className="mr-3 font-semibold hover:text-white text-[#ADB7BE] border-purple-500">Skills</button>
   )
