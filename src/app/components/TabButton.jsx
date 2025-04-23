b303d7e49738d55ea0d7852de79255d094417d96
Isnendyan <Isnendyankp@gmail.com>
2025-04-23 16:45:15 +0700

    feat:move passing children inside new paragraph at button component

--- src/app/components/TabButton.jsx
+++ src/app/components/TabButton.jsx
@@ -4,8 +4,8 @@ const TabButton = ({ active, selectTab, children }) => {
     const buttonClasses = active ? "text-white" : "text-[#ADB7BE]";
 
   return (
-    <button className={`mr-3 font-semibold hover:text-white ${buttonClasses} border-purple-500`}>
-        {children}
+    <button >
+        <p className={`mr-3 font-semibold hover:text-white ${buttonClasses} border-purple-500`}>{children}</p>
     </button>
   )
 }
