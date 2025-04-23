7d6a4f480007b18c18ebb8cd088c2acabb91bd0e
Isnendyan <Isnendyankp@gmail.com>
2025-04-23 16:51:42 +0700

    feat:passing tab button into aboutsection

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -3,6 +3,7 @@
 import React from 'react'
 import Image from 'next/image'
 import { useState, useTransition } from 'react'
+import TabButton from './TabButton'
 
 const AboutSection = () => {
   const [tab, setTab] = useState("skills")
@@ -29,6 +30,7 @@ const AboutSection = () => {
                 </p>
                 {/* Skills, Education, Experience */}
                 <div className="flex flex-row mt-8">
+                    <TabButton></TabButton>
                     <span className="mr-3 font-semibold hover:text-white text-[#ADB7BE] border-purple-500">Skills</span>
                     <span>Education</span>
                     <span>Experience</span>
