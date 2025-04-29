84e172caf2082b4317b46bf05228119c985299e1
Isnendyan <Isnendyankp@gmail.com>
2025-04-29 19:29:03 +0700

    feat: Add background image styling with dynamic URL in ProjectCard component

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const ProjectCard = () => {
   return (
     <div>
-        <div className="h-52 md:h-72">
+        <div 
+            className="h-52 md:h-72"
+            style={{ background: `url(${imgUrl})`, backgroundSize: "cover" }}
+        >
 
         </div>
     </div>
