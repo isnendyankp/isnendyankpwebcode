d4d3e2d6fc0853833ad65cec22e7bf743e6e6ac9
Isnendyan <Isnendyankp@gmail.com>
2025-05-02 16:00:38 +0700

    feat: Add padding to project card container for better spacing

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -9,7 +9,7 @@ const ProjectCard = ({imgUrl, title}) => {
             style={{ background: `url(${imgUrl})`, backgroundSize: "cover" }}
         >
         </div>
-        <div className="text-white rounded-b-xl bg-[#181818]">
+        <div className="text-white rounded-b-xl bg-[#181818]py-6 px-4">
             {/* Title */}
             <h5 className="text-xl font-semibold mb-2">{title}</h5>
             {/* Description */}
