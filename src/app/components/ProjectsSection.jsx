9a8fceff50b26f155213b565442d9c813dca6a9f
Isnendyan <Isnendyankp@gmail.com>
2025-05-12 16:08:09 +0700

    feat: Add padding to project filter buttons container for better spacing

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -63,7 +63,7 @@ const ProjectsSection = () => {
     {/* Projects */}
     <h2>My Projects</h2>
     {/* Filter */}
-    <div className="text-white flex flex-row justify-center items-center gap-2">
+    <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
         <button>ALL</button>
         <button>Web</button>
     </div>
