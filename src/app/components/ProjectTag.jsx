1d855b7e9c6a54ebba76fabbfbe3dbcae3638b1a
Isnendyan <Isnendyankp@gmail.com>
2025-05-14 15:04:53 +0700

    feat: Add props for ProjectTag component with name, onClick, and isSelected parameters

--- src/app/components/ProjectTag.jsx
+++ src/app/components/ProjectTag.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const ProjectTag = () => {
+const ProjectTag = ({ name, onClick, isSelected }) => {
   return (
     <button className="rounded-full border-2 border-purple-500 px-6 py-3 text-xl cursor-pointer">ALL</button>
   )
