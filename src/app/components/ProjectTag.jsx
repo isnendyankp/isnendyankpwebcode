264d46ff0a4a5b955ce614b9886e049e53cf6835
Isnendyan <Isnendyankp@gmail.com>
2025-05-15 16:21:00 +0700

    feat: Add dynamic button styles to ProjectTag component for better UI customization

--- src/app/components/ProjectTag.jsx
+++ src/app/components/ProjectTag.jsx
@@ -6,7 +6,7 @@ const ProjectTag = ({ name, onClick, isSelected }) => {
     : "text-[#ADB7BE]border-slate-600 hover:border-white";
 
     return (
-        <button className="rounded-full border-2 border-purple-500 px-6 py-3 text-xl cursor-pointer">ALL</button>
+        <button className={`${buttonStyles} rounded-full border-2 px-6 py-3 text-xl cursor-pointer`}>ALL</button>
     )
 }
 
