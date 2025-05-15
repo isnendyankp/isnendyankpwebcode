07bd327deb88f89502e2ee9e6593e907e2a86289
Isnendyan <Isnendyankp@gmail.com>
2025-05-15 16:17:23 +0700

    feat: Fix border color for unselected project tags to slate-600

--- src/app/components/ProjectTag.jsx
+++ src/app/components/ProjectTag.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ProjectTag = ({ name, onClick, isSelected }) => {
     const buttonStyles = isSelected
     ? "text-white border-primary-500"
-    : "text-[#ADB7BE]";
+    : "text-[#ADB7BE]border-slate-600";
 
     return (
         <button className="rounded-full border-2 border-purple-500 px-6 py-3 text-xl cursor-pointer">ALL</button>
