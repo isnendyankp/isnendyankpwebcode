2a4967a649b8136a994d6cf5eaadc9a1b4c9d28a
Isnendyan <Isnendyankp@gmail.com>
2025-05-18 20:43:58 +0700

    feat: Add grid layout and section structure for email component

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const EmailSection = () => {
   return (
-    <div>EmailSection</div>
+    <section className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4">
+        <div>
+
+        </div>
+    </section>
   )
 }
 
