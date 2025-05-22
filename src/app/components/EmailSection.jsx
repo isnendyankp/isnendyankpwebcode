b104da36cc0a7e7dd4dfd1d8053050efa6b534c5
Isnendyan <Isnendyankp@gmail.com>
2025-05-22 17:30:13 +0700

    feat: Add flex styling to socials div for horizontal icon alignment

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -10,7 +10,7 @@ const EmailSection = () => {
             <button className="text-[#ADB7BE] hover:text-white">Contact Me</button>
 
             {/* icon social contact */}
-            <div className="socials">
+            <div className="socials flex flex-row">
             <Link href="github.com"></Link>
 
             </div>
