e84b6c45af8af26348bfb4b5cc45fb47184fcea1
Isnendyan <Isnendyankp@gmail.com>
2025-05-25 19:38:29 +0700

    feat: Add GitHub profile link with target blank and noopener noreferrer attributes

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 import GithubIcon from "../../../public/github-icon.svg";
 import LinkedinIcon from "../../../public/linkedin-icon.svg";
-
+ 
 const EmailSection = () => {
   return (
     <section className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4">
@@ -14,7 +14,7 @@ const EmailSection = () => {
 
             {/* icon social contact */}
             <div className="socials flex flex-row gap-2">
-                <Link href="github.com">
+                <Link href="https://github.com/isnendyankp" target="_blank" rel="noopener noreferrer">
                     <Image src={GithubIcon} alt="Github Icon" />
                 </Link>
                 <Link href="linkedin.com">
