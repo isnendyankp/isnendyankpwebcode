beb0e598294f7b5c77e4afb04ecf22a6c4f9e470
Isnendyan <Isnendyankp@gmail.com>
2025-05-26 15:49:33 +0700

    feat: Update contact section with job seeking status and role preferences

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -9,9 +9,12 @@ const EmailSection = () => {
     <section className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4">
         <div>
             <h5 className="text-xl font-bold text-white my-2">Let's Connect</h5>
-            <p className="text-[#ADB7BE] mb-4 max-w-md"> Let's connect and let's build something great together</p>
+            <p className="text-[#ADB7BE] mb-4 max-w-md">
+              I&apos;m currently seeking roles as a Front-end Developer and QA Automation Engineer. Feel free to connect with me!
+            </p>
             
             {/* icon social contact */}
+            
             <div className="socials flex flex-row gap-2">
                 <Link href="https://github.com/isnendyankp" target="_blank" rel="noopener noreferrer">
                     <Image src={GithubIcon} alt="Github Icon" />
