15d6598d3fd4117addfc37288f6c7dfc0f7cbddb
Isnendyan <Isnendyankp@gmail.com>
2025-06-01 15:48:29 +0700

    feat: Update gradient color from purple-800 to primary-900 in EmailSection

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -11,9 +11,7 @@ const EmailSection = () => {
       className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative"
     >
         {/* Animate from left */}
-        <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-800 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-y-1/2">
-        </div>
-
+        <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
         {/* Contact section */}
         <div className="z-10">
             <h5 className="text-xl font-bold text-white my-2">
