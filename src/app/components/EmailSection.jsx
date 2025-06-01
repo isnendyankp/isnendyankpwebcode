ecbe32aa80d839061ab8bfb2ae3644926a573f89
Isnendyan <Isnendyankp@gmail.com>
2025-06-01 15:45:40 +0700

    feat: Add contact section ID for email component navigation

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -6,7 +6,10 @@ import LinkedinIcon from "../../../public/linkedin-icon.svg";
  
 const EmailSection = () => {
   return (
-    <section className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative">
+    <section
+      id="contact"
+      className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative"
+    >
         {/* Animate from left */}
         <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-800 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-y-1/2">
         </div>
