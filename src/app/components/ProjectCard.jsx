e9dc86ec1fcbb02337f6bfd0845dfc50009527c9
Isnendyan <Isnendyankp@gmail.com>
2025-06-12 17:12:49 +0700

    feat: Add title attributes to project links for better accessibility

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -17,12 +17,12 @@ const ProjectCard = ({imgUrl, title, description, gitUrl, deployUrl, previewUrl}
         >
             <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 ">  
                 {/* Code Icon */}
-                <Link href={gitUrl} target="_blank" rel="noopener noreferrer" className="h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link">
+                <Link href={gitUrl} target="_blank" rel="noopener noreferrer" title="GitHub Source" className="h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link">
                     <CodeBracketIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-white" />
                 </Link>
                 {/* Eye Icon */}
                 {deployUrl && (
-                  <Link href={deployUrl} target="_blank" rel="noopener noreferrer" className="h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link">
+                  <Link href={deployUrl} target="_blank" rel="noopener noreferrer" title="Deploy Website" className="h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link">
                       <EyeIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-white" />
                   </Link>
                 )}
