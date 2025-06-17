f0e5fa4e05477967f59bec5fffbfa0e376a78cf0
Isnendyan <Isnendyankp@gmail.com>
2025-06-17 17:31:58 +0700

    feat: Fix image path for Animated Template project and rename file

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -43,7 +43,7 @@ const projectsData = [
       id: 3,
       title: "Animated Template",
       description: "In this project, I learn how to use AOS (animation on scroll) & add inside HTML. And also this project uses Unsplash images and random user images.",
-      image: "/images/projects/Animated_Template.png",
+      image: "/images/projects/Animatedtemplate.png",
       tag: ["All", "Web"],
       deployUrl: "https://isnendyankp.github.io/animatedtemplate.github.io/",
       gitUrl: "https://github.com/isnendyankp/animatedtemplate.github.io",
