3d4e1c114435fa864bbd8f60a9844ff1308dc05f
Isnendyan <Isnendyankp@gmail.com>
2025-07-17 16:09:51 +0700

    feat: Add new project template with default values and placeholder links

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -220,6 +220,24 @@ const projectsData = [
         deployUrl: "https://isnendyankp.github.io/nasaapod.github.io/",
         gitUrl: "https://github.com/isnendyankp/nasaapod.github.io",
       },
+      {
+        id: 13,
+        title: "",
+        description: {
+          intro: "Default description",
+          features: [
+            "Default features",
+            "Default features",
+            "Default features",
+            "Default features"
+          ],
+          outro: "Default outro"
+        },
+        image: "/images/projects/.png",
+        tag: ["All", "Bootcamp Project"],
+        deployUrl: "https://isnendyankp.github.io/musicplayer.github.io/",
+        gitUrl: "https://github.com/isnendyankp/musicplayer.github.io",
+      },
   ];
 
 const ProjectsSection = () => {
