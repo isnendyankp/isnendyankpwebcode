959c94ca32149fc74fbf238eacf3ec89ec6aa838
Isnendyan <Isnendyankp@gmail.com>
2025-07-22 18:23:32 +0700

    fix: update deployment URL for Picture in Picture project

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -253,7 +253,7 @@ const projectsData = [
         },
         image: "/images/projects/.png",
         tag: ["All", "Mini Project"],
-        deployUrl: "https://isnendyankp.github.io/musicplayer.github.io/",
+        deployUrl: "https://isnendyankp.github.io/PictureInPicture.github.io/",
         gitUrl: "https://github.com/isnendyankp/PictureInPicture.github.io/",
       },
   ];
